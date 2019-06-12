b5a4657444da4f8a165355bd0e0aa15bbc32bd29
cai <181424167@qq.com>
2019-06-12 17:53:13 +0800

    no message

---
+++ dearcc-manager-web/src/main/java/com/wolf/dearcc/manager/core/shiro/filter/MyAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.wolf.dearcc.manager.core.shiro.filter;
+
+import com.wolf.dearcc.common.model.ApiResult;
+import org.apache.shiro.web.filter.AccessControlFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class MyAuthenticationFilter extends AccessControlFilter {
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object o) throws Exception {
+        if (request instanceof HttpServletRequest) {
+            if (((HttpServletRequest) request).getMethod().toUpperCase().equals("OPTIONS")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        ShiroFilterUtils.out(response, ApiResult.Fail("权限不足！"));
+        return false;
+    }
+}
